2b3152f9af35d2611d92852d64880bc12b8e94cb
Allan <allanl_95@hotmail.com>
2020-09-29 13:09:26 -0700

    contact-info

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -9,7 +9,30 @@ const Contact = () => {
 				<h1>Get in touch.</h1>
 			</div>
 
-			<div className='clients'>test</div>
+			<div className='contact-info'>
+				<div className='head-office'>
+					<p className='head-text'>Head Office:</p>
+					<p>
+						330-2608 Granville Street
+						<br/>
+						Vancouver, British Columbia
+						<br />
+						V6H 3V3 Canada
+					</p>
+				</div>
+				<div className='phone-email'>
+					<div className='phone'>
+						<p>
+							<b>Vancouver:</b> <span className='van'>+1 (604) 729-5787</span>
+							<br />
+							<b>Toronto:</b> <span className='toronto'>+1 (647) 632-2772</span>
+						</p>
+					</div>
+					<div className='email'>
+						<p>info@bamdigital.com</p>
+					</div>
+				</div>
+			</div>
 
 		</section>
 	);
